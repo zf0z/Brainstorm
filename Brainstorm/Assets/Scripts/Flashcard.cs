aa6beb982fbb3acb4bae484459535f0b2f28baff
Zachary Fozzard <zacharyfozz@gmail.com>
2023-03-17 18:37:01 +0000

    add flashcards to topics page

---
+++ Brainstorm/Assets/Scripts/Flashcard.cs
@@ -0,0 +1,7 @@
+public class Flashcard
+{
+    public long Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public long TopicId { get; set; }
+}
